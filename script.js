d2a92a1a360594c5a10e7b4c9fcb2011e2fb33a3
sonali698 <131346169+sonali698@users.noreply.github.com>
2023-04-20 13:53:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function dollersToRs(){
+    let currency = document.getElementById("currency").value;
+    let result = currency * 81.59;
+    document.getElementById("result").innerText="Result: Rs. "+result;
+}
+
+
+/* calculator.net
+css.glass (glass effect)-> copy the code 
+prasad surse
+*/
